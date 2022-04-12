3909f6c75c24efdcec161fe2d84faeca9891d45a
ohwehoh <ohwehoh@naver.com>
2022-04-12 13:52:02 +0900

    22.04.12.

---
+++ portfolio/react2022/src/components/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Contact(){
+    return (
+        <div>
+            여기는 contact 페이지입니다.
+        </div>
+    )
+}
+export default Contact;
---
+++ portfolio/react2022/src/components/pages/Youtube.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Youtube(){
+    return (
+        <div>
+            여기는 youtube 페이지입니다.
+        </div>
+    )
+}
+export default Youtube;
