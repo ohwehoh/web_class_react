64fe9b75463a248dba63bbdfb60d84446cafee9e
ohwehoh <ohwehoh@naver.com>
2022-04-14 09:10:32 +0900

    22.04.13.

---
+++ react2022/src/components/includes/MainCont.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MainCont(){
+    return(
+        <section className="main__cont">
+            <div className="main__inner">
+                <div>WE PROVIDE</div>
+                <div>VISUAL CODING</div>
+                <div>SOLUTIONS</div>
+                <div>FOR YOU WEBS</div>
+            </div>
+        </section>
+    )
+}
+
+export default MainCont;
