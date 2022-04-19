0c22b9cab41034e83245315434112242998025f5
ohwehoh <ohwehoh@naver.com>
2022-04-19 13:13:01 +0900

    04.19.

--- react2022/src/components/includes/PortCont.js
+++ react2022/src/components/includes/PortCont.js
@@ -2,92 +2,54 @@ import React from "react";
 
 //함수형 컨퍼넌트 ++클래스 컨퍼넌트-->리액트 훅
 
-function PortCont(){
+function PortInfo({image, title, category}){
+    return (
+        <article className="port__item">
+            <figure className="img">
+                <a href="/"><img src={image} alt="port1" /></a>
+            </figure>
+            <div className="text">
+                <h3>{title}</h3>
+                <p>{category}</p>
+            </div>
+        </article>
+    )
+}
+
+// const portText = [
+//     {
+//         image : "/img/port01@2x.jpg",
+//         alt : "port1",
+//         title : "Portfolio Title",
+//         desc : "Portfolio"
+//     },
+//     {
+//         image : "/img/port01@2x.jpg",
+//         alt : "port1",
+//         title : "Portfolio Title",
+//         desc : "Portfolio"
+//     },
+//     {
+//         image : "/img/port01@2x.jpg",
+//         alt : "port1",
+//         title : "Portfolio Title",
+//         desc : "Portfolio"
+//     },
+// ]
+
+function PortCont({port}){
     return(
         <section className="port__cont">
             <div className="container">
                 <div className="port__inner">
-                    <article className="port__item">
-                        <figure className="img">
-                            <a href="/"><img src="/img/port01@2x.jpg" alt="port1" /></a>
-                        </figure>
-                        <div className="text">
-                            <h3>Portfolio Title</h3>
-                            <p>Portfolio</p>
-                        </div>
-                    </article>
-                    <article className="port__item">
-                        <figure className="img">
-                            <a href="/"><img src="/img/port02@2x.jpg" alt="port1" /></a>
-                        </figure>
-                        <div className="text">
-                            <h3>Portfolio Title</h3>
-                            <p>Portfolio</p>
-                        </div>
-                    </article>
-                    <article className="port__item">
-                        <figure className="img">
-                            <a href="/"><img src="/img/port03@2x.jpg" alt="port1" /></a>
-                        </figure>
-                        <div className="text">
-                            <h3>Portfolio Title</h3>
-                            <p>Portfolio</p>
-                        </div>
-                    </article>
-                    <article className="port__item">
-                        <figure className="img">
-                            <a href="/"><img src="/img/port01@2x.jpg" alt="port1" /></a>
-                        </figure>
-                        <div className="text">
-                            <h3>Portfolio Title</h3>
-                            <p>Portfolio</p>
-                        </div>
-                    </article>
-                    <article className="port__item">
-                        <figure className="img">
-                            <a href="/"><img src="/img/port02@2x.jpg" alt="port1" /></a>
-                        </figure>
-                        <div className="text">
-                            <h3>Portfolio Title</h3>
-                            <p>Portfolio</p>
-                        </div>
-                    </article>
-                    <article className="port__item">
-                        <figure className="img">
-                            <a href="/"><img src="/img/port03@2x.jpg" alt="port1" /></a>
-                        </figure>
-                        <div className="text">
-                            <h3>Portfolio Title</h3>
-                            <p>Portfolio</p>
-                        </div>
-                    </article>
-                    <article className="port__item">
-                        <figure className="img">
-                            <a href="/"><img src="/img/port01@2x.jpg" alt="port1" /></a>
-                        </figure>
-                        <div className="text">
-                            <h3>Portfolio Title</h3>
-                            <p>Portfolio</p>
-                        </div>
-                    </article>
-                    <article className="port__item">
-                        <figure className="img">
-                            <a href="/"><img src="/img/port02@2x.jpg" alt="port1" /></a>
-                        </figure>
-                        <div className="text">
-                            <h3>Portfolio Title</h3>
-                            <p>Portfolio</p>
-                        </div>
-                    </article>
-                    <article className="port__item">
-                        <figure className="img">
-                            <a href="/"><img src="/img/port03@2x.jpg" alt="port1" /></a>
-                        </figure>
-                        <div className="text">
-                            <h3>Portfolio Title</h3>
-                            <p>Portfolio</p>
-                        </div>
-                    </article>
+                    {port.map((txt) => (
+                        <PortInfo
+                            key = {txt.title}
+                            image = {txt.image}
+                            title = {txt.title}
+                            category = {txt.category}
+                        />
+                    ))}
                 </div>
             </div>
         </section>
--- react2022/src/components/pages/Main.js
+++ react2022/src/components/pages/Main.js
@@ -1,19 +1,102 @@
-import React from "react";
+import React, { Children } from "react";
 import Header from "../layout/Header";
 import Contents from "../layout/Contents";
 import Footer from "../layout/Footer";
 import MainCont from "../includes/MainCont";
+import Loading from "../basics/Loading";
+import { gsap } from "gsap";
+import { checkPropTypes } from "prop-types";
 
-function Main(){
-    return (
-        <>
-            <Header />
-            <Contents>
-                <MainCont />
-            </Contents>
-            <Footer/>
-        </>
-    )
+// function Main(){
+//     return (
+//         <>
+//             <Header />
+//             <Contents>
+//                 <MainCont />
+//             </Contents>
+//             <Footer/>
+//         </>
+//     )
+// }
+
+class Main extends React.Component {
+    state = {
+        isLoading: true
+    }
+
+    mainAnimation = () => {
+        gsap.set(".main__inner", {opacity: 0})
+    }
+
+    getSite = () => {
+        setTimeout(()=>{
+            gsap.to("#header", {
+                duration: 0.8,
+                top: 0,
+            });
+            gsap.to("#footer", {
+                duration: 0.8,
+                bottom: 0,
+                delay: 0.4
+            });
+            gsap.to(".main__inner > div:nth-child(1)", {
+                duration: 0.6,
+                opacity: 1,
+                y: 0,
+                delay: 1.2,
+                ease: "slow(0.7, 0.7, false)"
+            });
+            gsap.to(".main__inner > div:nth-child(2)", {
+                duration: 0.6,
+                opacity: 1,
+                y: 0,
+                delay: 1.6,
+                ease: "slow(0.7, 0.7, false)"
+            });
+            gsap.to(".main__inner > div:nth-child(3)", {
+                duration: 0.6,
+                opacity: 1,
+                y: 0,
+                delay: 2.0,
+                ease: "slow(0.7, 0.7, false)"
+            });
+            gsap.to(".main__inner > div:nth-child(4)", {
+                duration: 0.6,
+                opacity: 1,
+                y: 0,
+                delay: 2.4,
+                ease: "slow(0.7, 0.7, false)"
+            });
+        }, 500)
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({isLoading:false});
+            // this.mainAnimation();...로딩이 다 안돼서 .main__inner를 불러오지 못한다.
+            this.getSite();
+        }, 3000)
+    }
+
+    render(){
+        const {isLoading} = this.state;
+
+        return (
+            <>
+                {isLoading ? (
+                    <Loading />
+                ) : (
+                    <>
+                        <Header />
+                        <Contents>
+                            <MainCont />
+                        </Contents>
+                        <Footer/>
+                    </>
+                )}
+            </>
+        )
+    }
 }
 
 export default Main;
